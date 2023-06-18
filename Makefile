ccbe4844741c2117f2557a73ab24d9129a065f22
JideOgunlana <jide611@gmail.com>
2023-06-18 10:29:24 +0200

    added script for env file

--- Makefile
+++ Makefile
@@ -1,11 +1,13 @@
 name = inception
 all:
 	@printf "Launch configuration ${name}...\n"
+	@bash srcs/requirements/tools/make_env.sh
 	@bash srcs/requirements/wordpress/tools/make_dir.sh
 	@docker-compose -f ./srcs/docker-compose.yml --env-file srcs/.env up -d
 
 build:
 	@printf "Building configuration ${name}...\n"
+	@bash srcs/requirements/tools/make_env.sh
 	@bash srcs/requirements/wordpress/tools/make_dir.sh
 	@docker-compose -f ./srcs/docker-compose.yml --env-file srcs/.env up -d --build
 
---
+++ srcs/requirements/tools/make_env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! -f "/home/${USER}/srcs/.env" ]; then
+	touch /home/${USER}/srcs/.env
+	echo DOMAIN_NAME=bogunlan.42.fr >> /home/${USER}/srcs/.env
+	echo CERT_=./requirements/tools/bogunlan.42.fr.crt >> /home/${USER}/srcs/.env
+	echo KEY_=./requirements/tools/bogunlan.42.fr.key >> /home/${USER}/srcs/.env
+	echo DB_NAME=wordpress >> /home/${USER}/srcs/.env
+	echo DB_ROOT=Born2beroot >> /home/${USER}/srcs/.env
+	echo DB_USER=wpuser >> /home/${USER}/srcs/.env
+	echo DB_PASS=Born2beroot >> /home/${USER}/srcs/.env
+fi
+
